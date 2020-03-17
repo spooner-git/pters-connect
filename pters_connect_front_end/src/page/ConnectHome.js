b3ac2acca46f79bacb0bf831072bd9a50c98f809
SK.Kim <skkim@spooner.co.kr>
2020-03-17 16:44:42 +0900

    메인 홈 페이지 소개 내용작성

--- pters_connect_front_end/src/page/ConnectHome.js
+++ pters_connect_front_end/src/page/ConnectHome.js
@@ -8,6 +8,8 @@ import ImageButton from '../component/Buttons/ImageButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRunning, faMusic } from '@fortawesome/free-solid-svg-icons';
 import Footer from '../component/Footer';
+import Title from '../component/Title';
+import Button from '../component/Buttons/Button';
 
 class PAGEConnectHome extends Component {
     constructor(props){
@@ -58,11 +60,11 @@ class PAGEConnectHome extends Component {
                     </h2>
                 </section>
 
-                <section className="section_at_home" style={{padding:"20px 0"}}>
+                <section className="section_at_home" style={{padding:"20px 0", paddingBottom:"0"}}>
                     <h2 style={{textAlign:"center", fontSize:"30px", fontWeight:"900", margin:0}}>
                         원하는 레슨을 선택해보세요!
                     </h2>
-                    <h3 style={{textAlign:"center", fontWeight:"normal", marginTop:0}}>간편하게 검색하고, 상담요청을 보내세요.</h3>
+                    <h3 style={{textAlign:"center", fontWeight:"normal", marginTop:0}}>간편하게 검색하고, 상담요청을 보내세요</h3>
                     <div className="home_category1_wrap">
                         {        
                             Object.keys(PROGRAM_CATEGORY).map((el)=>{
@@ -75,7 +77,7 @@ class PAGEConnectHome extends Component {
                                     icon =<FontAwesomeIcon icon={faMusic} style={{fontSize:"20px", marginRight:"5px"}}></FontAwesomeIcon>
                                 }
                                 let selected = code === this.state.category1 ? true : false;
-                                return <ButtonToggle onClick={()=>{this.handle_category1_set(code)}} highlight={selected} key={code} style={{borderRadius:"4px", height:"55px", border:0, fontSize:"16px"}} parent_style={{padding:0}}>
+                                return <ButtonToggle onClick={()=>{this.handle_category1_set(code)}} highlight={selected} key={code} style={{borderRadius:"2px", height:"55px", border:0, fontSize:"16px"}} parent_style={{padding:0}}>
                                             {icon}
                                             {name}
                                         </ButtonToggle>
@@ -83,7 +85,7 @@ class PAGEConnectHome extends Component {
                         }
                     </div>
                 </section>
-                <section className="section_at_home">
+                <section className="section_at_home" style={{padding:"20px 0", paddingTop:"0"}}>
                     <div className="home_category2_wrap">
                         {
                             Object.keys(PROGRAM_CATEGORY[this.state.category1].sub_category).map((el)=>{
@@ -91,10 +93,47 @@ class PAGEConnectHome extends Component {
                                 let code = el;
                                 return  <ImageButton key={code} onClick={()=>{this.handle_category2_set(code);this.go_url(`/list/?${this.state.category1}/?${code}`)}} image_url={`/images/category/category_image_${code}.jpg`} style={{display:"inline-block", width:"50%", height:"80px", border:"1px solid black", padding:"10px 20px"}}>
                                         <h3 style={{fontSize:"18px", fontWeight:"bold", color:"#ffffff", margin:"0"}}>{name}</h3>
-                                        {/* <span style={{float:"right", fontSize:"12px", fontWeight:"500", color:"#ffffff"}}>강사 123명</span> */}
                                     </ImageButton>
                             })
                         }
+                    </div>
+                </section>
+                
+                <section className="section_at_home" style={{padding:"30px 0", maxWidth:"unset", backgroundColor:"#f5f2f3"}}>
+                    <div className="home_flex_parent flex_reverse">
+                        <div>
+                            <img src="/images/front_page/pters_connect3.png" style={{width:"100%"}} className="front_image"></img>
+                            <div style={{paddingTop:"80%"}}></div>
+                        </div>
+                        <div style={{textAlign:"left"}}>
+                            <div style={{position:"absolute", top:"50%", transform:"translateY(-50%)"}}>
+                                <Title style={{fontSize:"28px", fontWeight:"900", padding:"10px 0", wordBreak:"keep-all"}}>레슨을 만나는 가장 쉬운 방법</Title>
+                                <Title style={{fontSize:"16px", fontWeight:"400", padding:"0", wordBreak:"keep-all"}}>좋은 강사님 어떻게 찾을지 고민 하지마세요.</Title>
+                                <Title style={{fontSize:"16px", fontWeight:"400", padding:"0", wordBreak:"keep-all"}}>간편하게 검색하고, 나와 맞는 강사님을 찾아 부담없이 상담할 수 있습니다.</Title>
+                                <Title style={{fontSize:"16px", fontWeight:"400", padding:"0", wordBreak:"keep-all"}}>지금 바로 시작해보세요!</Title>
+                                {/* <Button highlight={true} style={{border: "0", borderRadius:"3px", marginTop:"15px", padding:"0 15px", fontWeight:"500"}}>회원 가입</Button> */}
+                            </div>
+                            <div style={{paddingTop:"40%", minHeight:"250px"}}></div>
+                        </div>
+                    </div>
+                </section>
+                <section className="section_at_home" style={{padding:"30px 0", maxWidth:"unset", backgroundColor:"#fff"}}>
+                    <div className="home_flex_parent">
+                        <div style={{textAlign:"left"}}>
+                            <div style={{position:"absolute", top:"50%", transform:"translateY(-50%)"}}>
+                                <Title style={{fontSize:"28px", fontWeight:"900", padding:"10px 0", wordBreak:"keep-all"}}>수수료 없이 레슨 고객님과 연결</Title>
+                                <Title style={{fontSize:"16px", fontWeight:"400", padding:"0", wordBreak:"keep-all"}}>연결시마다 부담되는 수수료 부담 No!</Title>
+                                <Title style={{fontSize:"16px", fontWeight:"400", padding:"0", wordBreak:"keep-all"}}>내 프로필을 작성하고 레슨에만 집중해보세요!</Title>
+                                <Title style={{fontSize:"16px", fontWeight:"400", padding:"0", wordBreak:"keep-all"}}>상담이 도착했을 때 연락해 드릴께요.</Title>
+                                <Button highlight={true} style={{border: "0", borderRadius:"3px", marginTop:"15px", padding:"0 15px", fontWeight:"500"}}>강사로 가입</Button>
+                            </div>
+                            
+                            <div style={{paddingTop:"40%", minHeight:"250px"}}></div>
+                        </div>
+                        <div>
+                            <img src="/images/front_page/bg_big.png" style={{height:"100%", opacity:"0.8"}} className="front_image"></img>
+                            <div style={{paddingTop:"80%"}}></div>
+                        </div>
                     </div>
                 </section>
                 <Footer></Footer>
